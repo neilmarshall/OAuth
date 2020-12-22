88ec560c55b2599bc30f49243ee45b383bfb44ce
neilmarshall <neil.marshall@dunelm.org.uk>
2020-12-22 20:08:12 +0000

    add initial project structure

---
+++ SampleAPI/Controllers/PrimesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SampleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PrimesController : ControllerBase
+    {
+        private readonly ILogger<PrimesController> _logger;
+
+        public PrimesController(ILogger<PrimesController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Returns prime factors for a given number
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        [HttpGet("{n}")]
+        [ProducesResponseType(typeof(int[]), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult Factors(int n)
+        {
+            if (n < 0)
+            {
+                return BadRequest("Parameter must be a non-negative integer");
+            }
+
+            var factors = new List<int>();
+            for (int p = 2; p <= Math.Sqrt(n); p++)
+            {
+                while (n % p == 0)
+                {
+                    factors.Add(p);
+                    n /= p;
+                }
+            }
+
+            if (n > 1)
+            {
+                factors.Add(n);
+            }
+
+            return Ok(factors);
+        }
+    }
+}
---
+++ Tests/SampleAPI.Fixtures/BaseFixtures.cs
@@ -0,0 +1,57 @@
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleAPI.Fixtures
+{
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services => { });
+        }
+    }
+
+    [TestClass]
+    public class BaseFixtures
+    {
+        public static HttpClient httpclient;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            var webApplicationFactory = new CustomWebApplicationFactory<Startup>();
+            httpclient = webApplicationFactory.CreateClient();
+        }
+
+        [DataTestMethod]
+        [DataRow(0, new int[0])]
+        [DataRow(1, new int[0])]
+        [DataRow(2, new[] { 2 })]
+        [DataRow(3, new[] { 3 })]
+        [DataRow(25, new[] { 5, 5 })]
+        [DataRow(42, new[] { 2, 3, 7 })]
+        public async Task TestPrimesControllerFactorsActionReturnsOkForGoodInput(int n, int[] expectedFactors)
+        {
+            var responseObject = await httpclient.GetAsync($"primes/factors/{n}");
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, responseObject.StatusCode);
+
+            var actualFactors = JsonSerializer.Deserialize<int[]>(await responseObject.Content.ReadAsStringAsync());
+
+            CollectionAssert.AreEqual(expectedFactors, actualFactors);
+        }
+
+        [TestMethod]
+        public async Task TestPrimesControllerFactorsActionReturnsBadRequestForBadInput()
+        {
+            var responseObject = await httpclient.GetAsync("primes/factors/-3");
+
+            Assert.AreEqual(System.Net.HttpStatusCode.BadRequest, responseObject.StatusCode);
+        }
+    }
+}
